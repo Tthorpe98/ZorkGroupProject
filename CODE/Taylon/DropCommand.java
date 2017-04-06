f605e53a24858c5363b7eb907b3b01be208fda6e
Tthorpe98 <taylontyree@gmail.com>
2017-04-05 22:24:30 -0400

    Update

--- CODE/Taylon/DropCommand.java
+++ CODE/Taylon/DropCommand.java
@@ -1,12 +1,27 @@
 
+/**
+ * DropCommand extends the Command class and drops the given item from the inventory
+ * 
+ * @author Taylon Thorpe
+ * @version(4/5/17)
+ */
 class DropCommand extends Command {
 
     private String itemName;
 
+    /**
+     * Constructs a DropCommand with the given itemName
+     * @param itemName gives the name of the item to be dropped.
+     */
     DropCommand(String itemName) {
         this.itemName = itemName;
     }
 
+    /**
+     * Uses the itemName to get the item from the inventory and then drops it.
+     * @return String of the itemName that was dropped.
+     * @throws NoItemException
+     */
     public String execute() {
         if (itemName == null || itemName.trim().length() == 0) {
             return "Drop what?\n";
@@ -22,4 +37,3 @@ class DropCommand extends Command {
         }
     }
 }
-
