f91597605e02e16a9bac1b934a7fab5e67de43a3
Tthorpe98 <taylontyree@gmail.com>
2017-04-05 22:04:38 -0400

    Update Command.java

--- CODE/Taylon/Command.java
+++ CODE/Taylon/Command.java
@@ -1,4 +1,10 @@
 
+/**
+ * Command abstract that makes sure that all commands have an execute method
+ * 
+ * @author Taylon Thorpe
+ * @version(4/5/17)
+ */
 abstract class Command {
 
     abstract String execute();
